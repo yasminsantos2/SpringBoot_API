dbd49a7dac1d9037d15320f179636374559e76e3
Yasmin Santos <yasminsantos.eng@gmail.com>
2025-11-10 14:30:38 -0300

    feat: Validação com Bean Validation

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -15,11 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicoController {
 
     @Autowired
-    private MedicoRepository medicoRepository;
+    private MedicoRepository repository;
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        medicoRepository.save(new Medico(dados));   // ✅ igual ao vídeo
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+        repository.save(new Medico(dados));
     }
 }
